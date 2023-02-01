ddac6cf49d93666370ddd64cb290d3601cc3c9b3
Jennifer Henri <jenniferhkahn@gmail.com>
2023-02-01 15:07:05 -0500

    initial

---
+++ js/progress.js
@@ -0,0 +1,28 @@
+// JavaScript Document
+
+ $(document).ready(function() {
+ 
+  $('.method').on('click', function() {
+    $('.method').removeClass('blue-border');
+    $(this).addClass('blue-border');
+  });
+ 
+})
+
+.warning {
+  border-color: #f62f5e;
+}
+
+var $cardInput = $('.input-fields input');
+ 
+$('.next-btn').on('click', function(e) {
+ 
+  $cardInput.removeClass('warning');
+ 
+  $cardInput.each(function() {    
+     var $this = $(this);
+     if (!$this.val()) {
+       $this.addClass('warning');
+     }
+  })
+});
